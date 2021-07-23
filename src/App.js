d9a6b7b1fa157c13eb4d64d33d0c24a1342af5d4
Brannon Crumpton <bcrumpton72@gmail.com>
2021-07-22 20:39:11 -0500

    Added a submit button and some more details to the readme

--- src/App.js
+++ src/App.js
@@ -2,33 +2,44 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  // Initialize state for search term & results of fetch
+  const [query, setQuery] = useState('')
   const [results, setResults] = useState();
 
+  // On change of search input, throw value in state
   const handleChange = (e) => {
     const { value } = e.target
-    fetch(`https://api.giphy.com/v1/gifs/search?api_key=qb8j42lylFDFjJGU5yxhnota06h3liIj&q=${value}&lang=en&limit=25`)
+    
+    setQuery(value);
+  }
+
+  // On click of submit button, call Giphy API with search term
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=qb8j42lylFDFjJGU5yxhnota06h3liIj&q=${query}&lang=en&limit=25`)
       .then(res => res.json())
       .then(data => {
         setResults(data)
       })
   }
 
-  useEffect(() => {
-    console.log(results)
-  }, [results])
-
   return (
     <div className="App">
       <form method="GET">
         <label htmlFor="search">Giphy Search</label>
-        <input onChange={handleChange} type="search" name="search" id="search" placeholder="What would you like to search for?" />
+        <div className="field">
+          <input onChange={handleChange} type="search" name="search" id="search" placeholder="What would you like to search for?" />
+        </div>
+        <div className="field">
+          <input type="submit" onClick={handleSubmit} value="Search" />
+        </div>
       </form>
 
       <div className="results">
         {results && results.data.map((item) => {
           console.log(item)
           return (
-            <img src={item.images.fixed_width.url} />
+            <img src={item.images.fixed_width.url} alt={item.title} />
           )
         })}
       </div>
